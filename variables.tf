44db9ae510c969d47b80d2827ff474f1983d23d7
Veljko Joksimovic <vjoksimovic@griddynamics.com>
2022-09-02 13:33:25 +0200

    Trying to pass AWS credentials to Terraform - tweak #4

--- variables.tf
+++ variables.tf
@@ -3,8 +3,12 @@ variable "aws_region" {
   default = "eu-central-1"
 }
 
-variable "access" {
-  default = "aws-credentials"
+variable "access_key" {
+  default = "AWS_ACCESS_KEY_ID"
+}
+
+variable "secret_key" {
+  default = "AWS_SECRET_ACCESS_KEY"
 }
 
 variable "vpc_cidr_block" {
