5dba8500f57071bba7699436583804cca42624e8
Veljko Joksimovic <vjoksimovic@griddynamics.com>
2022-09-02 12:20:22 +0200

    AWS credentials error - tweak #3

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.aws_region
+  region                   = var.aws_region
+  shared_credentials_files = "~/.aws/credentials"
 }
 
 data "aws_availability_zones" "available" {
