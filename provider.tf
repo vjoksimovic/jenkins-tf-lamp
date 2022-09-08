a23f452c858463309f5b900c25ace00c28150de5
Veljko Joksimovic <vjoksimovic@griddynamics.com>
2022-09-08 10:36:43 +0200

    Tweaked shared_credentials_file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   profile                  = "veljo"
   region                   = var.aws_region
-  shared_credentials_files = ["~/.aws/credentials"]
+  shared_credentials_files = ["/root/.aws/credentials"]
 }
