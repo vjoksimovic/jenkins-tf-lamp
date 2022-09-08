c5568a5966ba1316cbc711779e9cb2088e71f1a5
Veljko Joksimovic <vjoksimovic@griddynamics.com>
2022-09-08 13:25:15 +0200

    AWS keys fix #7 - removed assume_role from provider

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region     = var.aws_region
   shared_credentials_files = ["/root/.aws/credentials"]
   shared_config_files = ["/root/.aws/config"]
-  assume_role {
-    role_arn = "arn:aws:iam::372462118821:role/jenkins-tf-lamp-role"
-  }
+#  assume_role {
+#    role_arn = "arn:aws:iam::372462118821:role/jenkins-tf-lamp-role"
+#  }
 }
