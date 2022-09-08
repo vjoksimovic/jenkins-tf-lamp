29db4e8d0562e859b8cba4629e6977a37eabcfab
Veljko Joksimovic <vjoksimovic@griddynamics.com>
2022-09-08 13:19:42 +0200

    AWS keys fix #5

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  profile    = "default"
+  profile    = ""
   region     = var.aws_region
+  shared_credentials_files = ["/root/.aws/credentials"]
+  shared_config_files = ["/root/.aws/config"]
   assume_role {
     role_arn = "arn:aws:iam::372462118821:role/jenkins-tf-lamp-role"
   }
